b191e0d4070e03fb8fb096c8ace1e53bd631034f
jachemharlyn <hmanlapaz@agsx.net>
2021-10-22 21:24:39 +0800

    added setup

---
+++ data extraction/word_detector_nn/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='word_detector_nn',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='https://github.com/Saphron-Asia/nan.ai-ml-ocr/tree/main/data%20extraction/word_detector_nn',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT License',
+      packages=['src','model'],
+      zip_safe=False)
